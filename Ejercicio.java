82ff0034d505675eab604db1f155a8bceef0e549
irma <irma.toscano@itszapopan.edu.mx>
2017-01-26 11:39:11 -0600

    ver 2

---
+++ Ejercicio.java
@@ -0,0 +1,10 @@
+public class Ejercicio {
+
+    public static void main (String [] args) {
+        int a=20;
+        int b=20;
+        int r=a+b;
+        System.out.println ("la suma es:  "+r);
+        System.out.println(" hola chicas");
+    }
+}
