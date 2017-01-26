e14375e620cbbdb214d15f36a2918fa3866ab8e9
irma <irma.toscano@itszapopan.edu.mx>
2017-01-26 11:43:38 -0600

    mas operaciones

--- Ejercicio.java
+++ Ejercicio.java
@@ -2,9 +2,10 @@ public class Ejercicio {
 
     public static void main (String [] args) {
         int a=20;
-        int b=20;
-        int r=a+b;
-        System.out.println ("la suma es:  "+r);
-        System.out.println(" hola chicas");
+        int b=40;
+        int s=a+b;
+        int r=b-a;
+        System.out.println ("la suma es:  "+s);
+        System.out.println(" la resta es"+r);
     }
 }
